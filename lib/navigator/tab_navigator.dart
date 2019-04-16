791f618cd0cf1d74093abefde2c8f2fee2b6d3d6
hai <448456376@qq.com>
2019-04-16 17:52:16 +0800

    第一次提交

---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
